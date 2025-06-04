a489b10371555e073ecd95e024f87d8a54a4aafa
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-06-05 02:06:09 +0300

    Configurate eslint

---
+++ server/eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["tests/**/*.js"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.node, ...globals.jest} } },
+  pluginReact.configs.flat.recommended,
+]);
