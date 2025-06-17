f9d0b4c7271b14b77f9545bb12b8e1d5c311a754
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-06-17 03:45:46 +0300

    patch

--- server/db/knexConfig.ts
+++ server/db/knexConfig.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 const isProd = process.env.NODE_ENV === "production";
-const migrationsDir = "./db/migrations";
+const migrationsDir = "./migrations";
 
 const config = {
   client: "pg",
