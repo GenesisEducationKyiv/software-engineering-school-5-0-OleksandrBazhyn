69104b4c108e3cc3ef61ae2c0629f4a7a40e3785
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-06-20 18:16:40 +0300

    fix

--- server/src/config.ts
+++ server/src/config.ts
@@ -10,5 +10,5 @@ export const config = {
   SMTP_PASS: required("SMTP_PASS", process.env.SMTP_PASS),
   SMTP_FROM: required("SMTP_FROM", process.env.SMTP_FROM),
   WEATHER_API_KEY: required("WEATHER_API_KEY", process.env.WEATHER_API_KEY),
-  DB_URL: required("DB_URL", process.env.DB_URL),
+  DATABASE_URL: required("DATABASE_URL", process.env.DB_URL),
 };
