911883fc9ab33c1f762ec90078b75b1c7018aaa6
OleksandrBazhyn <54002505+OleksandrBazhyn@users.noreply.github.com>
2025-07-27 20:04:55 +0300

    feat(metrics-service): initialize project with package.json, TypeScript configuration, and Prometheus setup (#31)
    - Added package.json with dependencies and devDependencies for the metrics service.
    - Created prometheus.yml for service monitoring configuration.
    - Set up tsconfig.json for TypeScript compilation settings, including strict type checking and module resolution.

--- email-service/src/router/index.ts
+++ email-service/src/router/index.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import { container } from "../di/container.js";
+import client from "prom-client";
+
 const router = Router();
 
 const emailController = container.emailController;
@@ -21,6 +23,13 @@ router.post("/send", async (req: Request, res: Response) => {
 
 router.get("/health", (req: Request, res: Response) => {
   res.status(200).json({ status: "Email service is healthy" });
+});
+
+client.collectDefaultMetrics();
+
+router.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 export default router;
